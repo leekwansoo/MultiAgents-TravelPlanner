1511e3e14f934254dc2bf422f018bbf9677b2d7a
leekwansoo <leekwansoo49@gmail.com>
2025-06-23 20:23:29 +0900

    Update generate_itinerary.py

--- modules/generate_itinerary.py
+++ modules/generate_itinerary.py
@@ -1,9 +1,11 @@
 from langchain_core.messages import HumanMessage
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 import json 
 
 def generate_itinerary(state):
-    llm = ChatOllama(model="llama3.2", base_url="http://localhost:11434")
+    # llm = ChatOllama(model="llama3.2", base_url="http://localhost:11434")
+    llm = ChatOpenAI(model="gpt-4o")
     prompt = f"""
     Using the following preferences, create a detailed itinerary:
     {json.dumps(state['preferences'], indent=2)}
