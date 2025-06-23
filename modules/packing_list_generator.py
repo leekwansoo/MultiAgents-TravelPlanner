70ba547fd3c0092a8f09bbf56f706af8684e7e8c
leekwansoo <leekwansoo49@gmail.com>
2025-06-23 20:28:39 +0900

    Update packing_list_generator.py

--- modules/packing_list_generator.py
+++ modules/packing_list_generator.py
@@ -1,8 +1,10 @@
 from langchain_core.messages import HumanMessage
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 def packing_list_generator(state):
-    llm = ChatOllama(model="llama3.2", base_url="http://localhost:11434")
+    # llm = ChatOllama(model="llama3.2", base_url="http://localhost:11434")
+    llm = ChatOpenAI(model="gpt-4o")
     prompt = f"""
     Generate a comprehensive packing list for a {state['preferences'].get('holiday_type', 'general')} holiday in {state['preferences'].get('destination', '')} during {state['preferences'].get('month', '')} for {state['preferences'].get('duration', 0)} days.
     Include essentials based on expected weather and trip type.
